7dd1d5563cc5f821c104051edf76c6bf9fe561b5
Louis <louis.piel@etu.unicaen.fr>
2023-09-06 10:46:38 +0200

    Initial commit

---
+++ script/script.js
@@ -0,0 +1,28 @@
+
+//Hello World
+/*let variable = document.getElementById("HelloWorld");
+variable.textContent = "Hello World";*/
+
+//Exercice1
+let celsius = 35;
+document.getElementById("Celsuis").textContent += celsius+ "C";
+let fahrenheit = celsius * 9/5 + 32;
+document.getElementById("Farent").textContent += fahrenheit+ "F";
+
+//Exercice2
+
+let longueur = 20;
+let largeur = 50;
+document.getElementById("Rectangle").textContent += longueur * largeur;
+
+//Exercice3
+
+let prenom = "Louis";
+let nom = "Piel";
+document.getElementById("Nom").textContent = prenom+ " "+nom;
+
+//Exercice4
+
+let HT = 15;
+
+
